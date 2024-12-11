90dce3c7edefb4b2ae9c0d6fe1bc6fc51bf931cf
Gabriel de Morais <gabrieldemorais6@gmail.com>
2024-12-11 03:24:14 -0300

    Add integration tests

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
-export * from "./args.ts";
 export * from "./calculation.ts";
 export * from "./calculation.types.ts";
 export * from "./constants.ts";
--- main_test.ts
+++ main_test.ts
@@ -1,1 +1,72 @@
-import "./lib/calculation.test.ts";
+import { assertEquals } from "@std/assert/equals";
+
+// To be able to run this test, run "deno task test" or "deno test --allow-read --allow-run".
+Deno.test(
+  "[capital gains calculation cli app] it should run CLI app gracefully from the terminal",
+  async () => {
+    // Arrange
+    const process = new Deno.Command(Deno.execPath(), {
+      args: ["run", "--quiet", "main.ts"],
+      stdin: "piped",
+      stdout: "piped",
+      stderr: "piped",
+    });
+
+    const input = `
+      [{"operation":"buy","unit-cost":10,"quantity":100}]
+      [{"operation":"buy","unit-cost":10,"quantity":100},{"operation":"sell","unit-cost":15,"quantity":50}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":20,"quantity":5000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":20,"quantity":5000},{"operation":"sell","unit-cost":5,"quantity":5000}]
+      [{"operation":"buy","unit-cost":10,"quantity":100},{"operation":"sell","unit-cost":15,"quantity":50},{"operation":"sell","unit-cost":15,"quantity":50}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":20,"quantity":5000},{"operation":"sell","unit-cost":5,"quantity":5000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":5,"quantity":5000},{"operation":"sell","unit-cost":20,"quantity":3000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"buy","unit-cost":25,"quantity":5000},{"operation":"sell","unit-cost":15,"quantity":10000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"buy","unit-cost":25,"quantity":5000},{"operation":"sell","unit-cost":15,"quantity":10000},{"operation":"sell","unit-cost":25,"quantity":5000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":2,"quantity":5000},{"operation":"sell","unit-cost":20,"quantity":2000},{"operation":"sell","unit-cost":20,"quantity":2000},{"operation":"sell","unit-cost":25,"quantity":1000}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":2,"quantity":5000},{"operation":"sell","unit-cost":20,"quantity":2000},{"operation":"sell","unit-cost":20,"quantity":2000},{"operation":"sell","unit-cost":25,"quantity":1000},{"operation":"buy","unit-cost":20,"quantity":10000},{"operation":"sell","unit-cost":15,"quantity":5000},{"operation":"sell","unit-cost":30,"quantity":4350},{"operation":"sell","unit-cost":30,"quantity":650}]
+      [{"operation":"buy","unit-cost":10,"quantity":10000},{"operation":"sell","unit-cost":50,"quantity":10000},{"operation":"buy","unit-cost":20,"quantity":10000},{"operation":"sell","unit-cost":50,"quantity":10000}]
+    `;
+
+    // IMPORTANT: This indentation was deliberately placed like this in order to correctly assert to the actual output.
+    const expectedOutput = `
+[
+  [ { tax: 0 } ],
+  [ { tax: 0 }, { tax: 0 } ],
+  [ { tax: 0 }, { tax: 10000 } ],
+  [ { tax: 0 }, { tax: 10000 }, { tax: 0 } ],
+  [ { tax: 0 }, { tax: 0 }, { tax: 0 } ],
+  [ { tax: 0 }, { tax: 10000 }, { tax: 0 } ],
+  [ { tax: 0 }, { tax: 0 }, { tax: 1000 } ],
+  [ { tax: 0 }, { tax: 0 }, { tax: 0 } ],
+  [ { tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 10000 } ],
+  [ { tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 3000 } ],
+  [
+    { tax: 0 },
+    { tax: 0 },
+    { tax: 0 },
+    { tax: 0 },
+    { tax: 3000 },
+    { tax: 0 },
+    { tax: 0 },
+    { tax: 3700 },
+    { tax: 0 }
+  ],
+  [ { tax: 0 }, { tax: 80000 }, { tax: 0 }, { tax: 60000 } ]
+]
+    `;
+
+    const command = process.spawn();
+    const writer = command.stdin.getWriter();
+    await writer.write(new TextEncoder().encode(input));
+    await writer.close();
+
+    // Act
+    const { code: exitCode } = await command.status;
+    const rawOutput = await command.output(); // stdout
+    const output = new TextDecoder().decode(rawOutput.stdout);
+
+    // Assert
+    assertEquals(exitCode, 0);
+    assertEquals(output.trim(), expectedOutput.trim());
+  }
+);
